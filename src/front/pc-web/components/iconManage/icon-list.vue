d2145d27352f5a696802944696c657c74464f563
roymtang <roymtang@gmail.com>
2018-04-28 15:34:49 +0800

    添加图标

--- src/front/pc-web/components/iconManage/icon-list.vue
+++ src/front/pc-web/components/iconManage/icon-list.vue
@@ -1,7 +1,12 @@
 <template>
     <div>
-        <icon name="dalianyinhang" scale="5"></icon>
-        <icon name="wujiaoxing" style="fill: blue;"></icon>
+        <icon name="dianzan" scale="5" fill=""></icon>
+        <icon name="dingwei" scale="5" fill="red"></icon>
+        <icon name="qiandao" scale="5" fill="#2F77F1"></icon>
+        <icon name="shangchuan" scale="5" fill="#2577FF"></icon>
+        <icon name="shoucang" scale="5" fill="#F7F1AA"></icon>
+        <icon name="yuyin" scale="5" fill="#2577FF"></icon>
+        <icon name="xuanzhuan" scale="5" fill="#2577FF" spin></icon>
     </div>
 </template>
 
